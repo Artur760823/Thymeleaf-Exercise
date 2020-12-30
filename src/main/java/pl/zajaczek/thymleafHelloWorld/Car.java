bc6e7b0f702ef6b75e63a8f9ebbfbc0aecc90b3f
Artur <artur.zajaczkowski76@gmail.com>
2020-12-30 10:22:34 +0100

    Add new class Car + geters and setters

--- src/main/java/pl/zajaczek/thymleafHelloWorld/Car.java
+++ src/main/java/pl/zajaczek/thymleafHelloWorld/Car.java
@@ -2,5 +2,22 @@ package pl.zajaczek.thymleafHelloWorld;
 
 public class Car {
 
-    private String Model
+    private String model;
+    private String mark;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
 }
