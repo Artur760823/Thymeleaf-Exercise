75ef01e9614b1bd0f58f7942158f09627aed2275
Artur <artur.zajaczkowski76@gmail.com>
2020-12-30 10:34:03 +0100

    rename all names to "Car"

--- src/main/java/pl/zajaczek/thymleafHelloWorld/CarController.java
+++ src/main/java/pl/zajaczek/thymleafHelloWorld/CarController.java
@@ -6,13 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class CarController {
 
-    @GetMapping("/hello")
+    @GetMapping("/car")
     public String get(Model model){
         Car car = new Car("BMW", "i8");
         model.addAttribute("name", "Artur");
         model.addAttribute("car", car);
-        return "hello";
+        return "car";
     }
 }
