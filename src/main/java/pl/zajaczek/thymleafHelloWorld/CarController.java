fa3b63036536a01e98ddaf1fb94b5e8215f40015
Artur <artur.zajaczkowski76@gmail.com>
2020-12-30 10:47:35 +0100

    Add ArrayList of cars and for each method to html

--- src/main/java/pl/zajaczek/thymleafHelloWorld/CarController.java
+++ src/main/java/pl/zajaczek/thymleafHelloWorld/CarController.java
@@ -5,14 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public String get(Model model){
         Car car = new Car("BMW", "i8");
-        model.addAttribute("name", "Artur");
-        model.addAttribute("car", car);
+        Car car1 = new Car("Fiat", "126p");
+        Car car2 = new Car("Polonez", "Caro");
+        Car car3 = new Car("Citroen", "C2");
+        Car car4 = new Car("Peugeot", "308SW");
+        Car car5 = new Car("Syrena", "Sport");
+
+        List<Car> cars = Arrays.asList(car, car1, car2, car3, car4, car5);
+
+        model.addAttribute("cars", cars);
         return "car";
     }
 }
